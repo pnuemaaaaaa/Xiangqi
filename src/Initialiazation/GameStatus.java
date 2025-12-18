b08911de21bd28ca37fcddd7460a820e4dd34f56
pnuemaaaaaa <3113790652@qq.com>
2025-12-18 14:21:20 +0800

    Delete src directory

--- src/Initialiazation/GameStatus.java
+++
@@ -1,9 +0,0 @@
-package Initialiazation;
-
-public enum GameStatus {
-    NORMAL,      // 正常状态
-    CHECK,       // 将军状态
-    CHECKMATE,   // 将死状态
-    STALEMATE,   // 困毙状态（和棋）
-    DRAW         // 和棋（其他情况）
-}
